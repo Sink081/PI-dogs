7ddaf32b5106245db684ba4f369828101514f581
unknown <berto908@hotmail.com.ar>
2023-02-17 10:40:31 -0300

    Apto para presentacion

--- client/src/Views/Dog-Details/Details.jsx
+++ client/src/Views/Dog-Details/Details.jsx
@@ -19,18 +19,20 @@ import style from "./Detail.module.css"
     return (
         <div className={style.detailsBackground}>
             <div className={style.detailsCss}>
+                
             <h2> {dogDetails.name} </h2>
             <div className={style.detailsContainer}>
                 <img src={dogDetails.image} alt= {"Loading..."} />
             </div>
-            {dogDetails.temperament ? <p> <b>Temperaments: </b> {dogDetails.temperament}</p> : null}
+            {dogDetails.temperament ? <p> <b>Temperaments: </b> {dogDetails.temperament.join(", ")}</p> : null}
             <div className={style.weightCss}>
                 <p>Weight : {dogDetails.weight} Kg.</p>
             </div>
             <p className={style.heightCss}>Height: {dogDetails.height} cm</p>
             <p className={style.lifeCss}>Life span: {dogDetails.life_span} </p>
-            <Link to= {"/home"}>
-                <button>Back </button>
+
+            <Link className={style.btnContainer} to= {"/home"}>
+                <button className={style.button}>Back </button>
             </Link>
             </div>
         </div>
